8e92043e0df02308b0e9f5c31e216bc6934740c2
petrovska-nika <petrovska.nika.dev@gmail.com>
2025-03-14 23:39:14 +0200

    hw2

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,13 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
 
-  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-  return totalWidth;
+  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
